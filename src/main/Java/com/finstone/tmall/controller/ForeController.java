a00e37f1b8cc4840fc56607b6989beebc8e51c31
uncle90 <yuzd2016@hotmail.com>
2019-08-01 23:29:41 +0800

    产品页

--- src/main/Java/com/finstone/tmall/controller/ForeController.java
+++ src/main/Java/com/finstone/tmall/controller/ForeController.java
@@ -1,13 +1,7 @@
 package com.finstone.tmall.controller;
 
-import com.finstone.tmall.entity.Category;
-import com.finstone.tmall.entity.Product;
-import com.finstone.tmall.entity.ProductImage;
-import com.finstone.tmall.entity.User;
-import com.finstone.tmall.service.CategoryService;
-import com.finstone.tmall.service.ProductImageService;
-import com.finstone.tmall.service.ProductService;
-import com.finstone.tmall.service.UserService;
+import com.finstone.tmall.entity.*;
+import com.finstone.tmall.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,26 +28,15 @@ public class ForeController {
     @Autowired
     ProductImageService productImageService;
 
+    @Autowired
+    PropertyValueService propertyValueService;
+
     /**
      * 首页
-     * 一、功能
      * 1. 在横向导航栏上提供4个分类连接
      * 2. 在纵向导航栏上提供全部17个分类连接
      * 3. 当鼠标移动到某一个纵向分类连接的时候，显示这个分类下的推荐产品列表
      * 4. 按照每种分类显示5个商品的方式，显示所有17种分类
-     * <p>
-     * 二、页面结构
-     * <%@include file="../include/fore/header.jsp"%>
-     * <%@include file="../include/fore/top.jsp"%>                      导航栏
-     * <%@include file="../include/fore/search.jsp"%>                   搜索框
-     * <%@include file="../include/fore/home/homePage.jsp"%>            页面主体
-     * <%@include file="categoryAndcarousel.jsp"%>                  导航和轮播、分类和产品推荐
-     * rightMenu：<a href="forecategory?cid=${c.id}">      导航
-     * <%@include file="categoryMenu.jsp" %>               商品分类
-     * <%@include file="productsAsideCategorys.jsp" %>     推荐商品（按类别分组）
-     * <%@include file="carousel.jsp" %>                   轮播
-     * <%@include file="homepageCategoryProducts.jsp"%>             产品列表
-     * <%@include file="../include/fore/footer.jsp"%>
      */
     @RequestMapping("forehome")
     public String home(Model model) {
@@ -71,13 +54,23 @@ public class ForeController {
         return "fore/home";
     }
 
-    //登录页
+    /**
+     * 登录页
+     * @return
+     */
     @RequestMapping("loginPage")
     public String loginPage(){
         return "fore/login";
     }
 
-    //登录
+    /**
+     * 登录
+     * @param model
+     * @param session
+     * @param name
+     * @param password
+     * @return
+     */
     @RequestMapping("forelogin")
     public String login(Model model, HttpSession session,
                         @RequestParam("name") String name,
@@ -94,21 +87,25 @@ public class ForeController {
         model.addAttribute("user",user);
         //添加session
         session.setAttribute("user", user);
-
-
-        //查询购物车
-        //查询订单
+        //TODO 查询购物车 查询订单
         return "redirect:forehome";
     }
 
-
-    //注册页
+    /**
+     * 注册页
+     * @return
+     */
     @RequestMapping("registerPage")
     public String register(){
         return "fore/register";
     }
 
-    //注册
+    /**
+     * 注册
+     * @param model
+     * @param user
+     * @return
+     */
     @RequestMapping("foreregister")
     public String foreregister(Model model, User user){
         //特殊字符转义
@@ -125,20 +122,32 @@ public class ForeController {
         return "redirect:registerSuccess";
     }
 
-    //注册成功页
+    /**
+     * 注册成功页
+     * @return
+     */
     @RequestMapping("registerSuccess")
     public String registerSuccess(){
         return "fore/registerSuccess";
     }
 
-    //退出
+    /**
+     * 退出
+     * @param session
+     * @return
+     */
     @RequestMapping("forelogout")
     public String logout(HttpSession session){
         session.removeAttribute("user");
         return "redirect:forehome";
     }
 
-    //产品页
+    /**
+     * 产品页
+     * @param pid
+     * @param model
+     * @return
+     */
     @RequestMapping("foreproduct")
     public String product(@RequestParam("pid") int pid, Model model){
         //分类list
@@ -153,11 +162,14 @@ public class ForeController {
         List<ProductImage> pisDetail = productImageService.list(pid, "type_detail");
         product.setProductDetailImages(pisDetail);
 
-        //产品评价
+        //商品详情（产品属性）
+        List<PropertyValue> pvs = propertyValueService.list(pid);
 
+        //TODO 产品评价
 
         model.addAttribute("cs",cs);
         model.addAttribute("p", product);
+        model.addAttribute("pvs",pvs);
         return "fore/product";
     }
 
