91048c43979bce245101382ccc4d2f5f8de8b388
uncle90 <yuzd2016@hotmail.com>
2019-08-06 00:01:14 +0800

    加入购物车

--- src/main/Java/com/finstone/tmall/controller/ShoppingController.java
+++ src/main/Java/com/finstone/tmall/controller/ShoppingController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -61,5 +62,41 @@ public class ShoppingController {
 
         return "redirect:forebuy?oiid="+orderItem.getOid();
     }
+
+
+    /**
+     * 把某种商品加入购物车。如果购物车有同类商品，则追加数量。与 forebuyone 类似。
+     * @return
+     */
+    @RequestMapping("foreaddCart")
+    @ResponseBody
+    public String foreaddCart(HttpSession session,
+                              @RequestParam("pid") int pid,
+                              @RequestParam("num") int num){
+        User user = (User) session.getAttribute("user");
+        //检查购物车是否有同类商品
+        boolean exist = false;
+        OrderItem orderItem = null;
+        List<OrderItem> ois = orderItemService.listByUser(user.getId());
+        if(ois!=null && !ois.isEmpty()){
+            for(OrderItem oi: ois){
+                if(oi.getPid()==pid){//自动拆箱
+                    orderItem = oi;
+                    exist = true;
+                    break;
+                }
+            }
+        }
+        //修改订单项
+        if(exist){
+            orderItem.setNumber(orderItem.getNumber()+num);
+            orderItemService.update(orderItem); //追加商品数量
+        }else{
+            orderItem = new OrderItem(pid, user.getId(), num);
+            orderItemService.add(orderItem);    //新建订单项
+        }
+        return "success";
+    }
+
 
 }
