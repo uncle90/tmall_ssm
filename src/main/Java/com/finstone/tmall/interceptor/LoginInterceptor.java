b5652ded9409861910429fe1eae583aef5126c84
uncle90 <yuzd2016@hotmail.com>
2019-08-11 16:44:25 +0800

    添加登录状态拦截器，不再通过session判断是否已登录

--- src/main/Java/com/finstone/tmall/interceptor/LoginInterceptor.java
+++ src/main/Java/com/finstone/tmall/interceptor/LoginInterceptor.java
@@ -70,7 +70,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
      */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        //System.out.println("postHandle......");
+        //System.out.println("postHandle1111......");
     }
 
     /**
@@ -79,6 +79,6 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
      */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        //System.out.println("afterCompletion......");
+        //System.out.println("afterCompletion1111......");
     }
 }
